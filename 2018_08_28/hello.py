865c37f000e76263eaace6fa54108084d1cb2c1c
wangzhen <599174211@qq.com>
2018-09-01 16:15:13 +0800

    删除东西

--- 2018_08_28/hello.py
+++
@@ -1,9 +0,0 @@
-print('hello python ')
-a = input('请输入一串字符')
-if a == 'wangzhen'  :
-    print('输入正确')
-    b = input('继续输入一串字符')
-    if b=='love' :
-        print('恭喜再次输入正确')
-else :
-    print('输入错误')
